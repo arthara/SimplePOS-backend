03f57eb2c8e62d90a6d3871c6e6c8d69036fce50
Re <refanopradana@gmail.com>
2021-02-17 07:45:45 +0700

    Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
             'picture' => 'image|mimes:jpeg,png,jpg|max:2048',
             'total' => 'required|numeric',
             'selling_price' => 'required|numeric',
-
+            'cost_price' => 'required|numeric'
         ]);
 
         if ($validator->fails()) {
@@ -37,29 +37,86 @@ class ProductController extends Controller
         //Checking File
         $uploadFile = $request->file('image');
         if ($uploadFile) {
-            $path = $uploadFile->store('public/img/health_agencies');
+            $path = $uploadFile->store('public/img/products');
         } else {
             $path = null;
         }
 
-        $health_agency = HealthAgency::create([
+        $product = Product::create([
             'name' => $request->name,
-            'address' => $request->address,
-            'image' => $path,
-            'call_center' => $request->call_center,
-            'email' => $request->email,
+            'picture' => $path,
+            'total' => $request->total,
+            'selling_price' => $request->selling_price,
+            'cost_price' => $request->cost_price,
         ]);
 
-        if ($health_agency)
+        if ($product)
             return response()->json([
                 'success' => true,
                 'message' => 'Add data successfully!',
-                'data' => $health_agency,
+                'data' => $product,
             ], 200);
         else
             return response()->json([
                 'success' => false,
                 'message' => 'Add data failed!',
+            ], 500);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\HealthAgency  $healthAgency
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, HealthAgency $healthAgency)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'address' => 'required|string',
+            'image' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'call_center' => 'required',
+            'email' => 'required|email|unique:health_agencies,email,' . $healthAgency->id,
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //Checking File
+        $uploadFile = $request->file('image');
+        if ($uploadFile != null) {
+            File::delete(storage_path('app/public/img/health_agencies/') . $healthAgency->image);
+            $path = $uploadFile->store('public/img/health_agencies');
+            $fileName = explode('/', $path);
+            $fileName = end($fileName);
+        } else {
+            $fileName = $healthAgency->image;
+        }
+
+        $isUpdate = HealthAgency::where('id', $healthAgency->id)
+            ->update([
+                'name' => $request->name,
+                'address' => $request->address,
+                'image' => $fileName,
+                'call_center' => $request->call_center,
+                'email' => $request->email,
+            ]);
+
+        $health_agency = HealthAgency::where('id', $healthAgency->id)->first();
+
+        if ($isUpdate)
+            return response()->json([
+                'success' => true,
+                'message' => 'Update data successfully!',
+                'data' => $health_agency,
+            ], 200);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Update data failed!',
+                'data' => $health_agency,
             ], 500);
     }
 }
