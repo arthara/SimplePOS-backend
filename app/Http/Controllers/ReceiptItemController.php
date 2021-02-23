c304b84899e2c80ffe10d4e9a00518a7ef4ca2e8
AanMFA <aanmfa06@gmail.com>
2021-02-23 10:09:09 +0700

    Buat fitur total sales per hari #25

--- app/Http/Controllers/ReceiptItemController.php
+++ app/Http/Controllers/ReceiptItemController.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Auth;
 
 class ReceiptItemController extends Controller
 {
-    public function dailySales($date){
-        $date = Carbon::parse($date);
+    public function dailySales($inputDate){
+        $date = Carbon::parse($inputDate);
         $store = Auth::user()->store;
 
         if(!$store)
@@ -19,21 +19,23 @@ class ReceiptItemController extends Controller
         $receipts = $store->receipt->whereBetween(
             'receipt_time', [$date->format('Y-m-d')." 00:00:00", $date->addDay()->format('Y-m-d')." 00:00:00"]
         );
-        $totalSales = 0;
+        $totalProfit = 0;
         $totalItem = 0;
 
         foreach($receipts as $receipt){
             foreach($receipt->receiptItem as $receiptItem){
-                $totalItem++;
-                $totalSales += ($receiptItem->unit_price * $receiptItem->product->cost_price) * $receiptItem->unit_total;
+                $totalItem += $receiptItem->unit_total;
+                //profit += total item * (sold-cost)
+                $totalProfit += $receiptItem->unit_total *
+                    ($receiptItem->product_history->selling_price - $receiptItem->product_history->cost_price) ;
             }
-            $totalSales += $receipt->other_charges - $receipt->discount;
+            $totalProfit += $receipt->other_charges - $receipt->discount + $receipt->tax;
         }
 
         return response()->json([
-            "Date" => $date,
+            "Date" => $inputDate,
             "Total Sales" => $totalItem,
-            "Laba Kotor" => $totalSales,
+            "Laba Kotor" => $totalProfit,
         ], 200);
     }
 }
--- app/Models/ReceiptItem.php
+++ app/Models/ReceiptItem.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class ReceiptItem extends BaseModel
 {
     public function receipt(){
         return $this->belongsTo(Receipt::class);
+    }
+
+    /**
+     * Get the product_history associated with the ReceiptItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product_history(): BelongsTo
+    {
+        return $this->belongsTo(ProductHistory::class);
     }
 }
