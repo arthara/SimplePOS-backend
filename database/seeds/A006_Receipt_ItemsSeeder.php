81b20e8aa1687c17a15a85658e78c3fbee918b3e
AanMFA <aanmfa06@gmail.com>
2021-02-23 09:26:10 +0700

    Tambah migrasi product history dan event otomatis Fixes #29

--- database/seeds/A006_Receipt_ItemsSeeder.php
+++ database/seeds/A006_Receipt_ItemsSeeder.php
@@ -12,12 +12,12 @@ class A006_Receipt_ItemsSeeder extends Seeder
     public function run()
     {
         $receipt_items = array(
-            array('id' => 1, 'receipt_id' => 1, 'product_id' => 1, 'unit_total' => 1, 'unit_price' => 2000.21, 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
-            array('id' => 2, 'receipt_id' => 1, 'product_id' => 2, 'unit_total' => 2, 'unit_price' => 1500.21, 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
-            array('id' => 3, 'receipt_id' => 2, 'product_id' => 1, 'unit_total' => 2, 'unit_price' => 2000.21, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
-            array('id' => 4, 'receipt_id' => 2, 'product_id' => 2, 'unit_total' => 1, 'unit_price' => 1500.21, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04')
+            array('id' => 1, 'receipt_id' => 1, 'product_history_id' => 1, 'unit_total' => 1, 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 2, 'receipt_id' => 1, 'product_history_id' => 2, 'unit_total' => 2, 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 3, 'receipt_id' => 2, 'product_history_id' => 1, 'unit_total' => 2, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 4, 'receipt_id' => 2, 'product_history_id' => 2, 'unit_total' => 1, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04')
         );
-        
+
         DB::table('receipt_items')->insert($receipt_items);
     }
 }
