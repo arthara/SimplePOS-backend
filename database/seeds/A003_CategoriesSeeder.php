e53de2d84b3a60ab2ce8bee509b53a5d81c4eb43
ARTHARA <the.first678@gmail.com>
2021-03-09 08:32:15 +0700

    Penambahan Seeder (#48)

--- database/seeds/A003_CategoriesSeeder.php
+++ database/seeds/A003_CategoriesSeeder.php
@@ -13,7 +13,20 @@ class A003_CategoriesSeeder extends Seeder
     {
         $categories = array(
             array('id' => 1, 'store_id' => 1, 'name' => 'Minuman', 'color' => '#ffffff','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
-            array('id' => 2, 'store_id' => 1, 'name' => 'Makanan', 'color' => '#ff0000','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03')
+            array('id' => 2, 'store_id' => 1, 'name' => 'Bahan Makanan Pokok', 'color' => '#ff0005','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 3, 'store_id' => 1, 'name' => 'Keperluan Peralatan Dapur', 'color' => '#a27172','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 4, 'store_id' => 1, 'name' => 'Keperluan Peralatan Rumah Tangga', 'color' => '#b5da62','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 5, 'store_id' => 1, 'name' => 'Pembersih Rumah', 'color' => '#4b8bfc','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 6, 'store_id' => 1, 'name' => 'Keperluan Bayi', 'color' => '#851313','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 7, 'store_id' => 1, 'name' => 'Perlengkapan Toilet', 'color' => '#310a67','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 8, 'store_id' => 1, 'name' => 'Kosmetik', 'color' => '#37b5a5','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 9, 'store_id' => 1, 'name' => 'Obat-Obatan', 'color' => '#706435','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 10, 'store_id' => 1, 'name' => 'Keperluan Memasak', 'color' => '#a31894','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 11, 'store_id' => 1, 'name' => 'Daging & Ikan', 'color' => '#edda05','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 12, 'store_id' => 1, 'name' => 'Alat Tulis & Kantor', 'color' => '#719105','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 13, 'store_id' => 1, 'name' => 'Bahan & Peralatan Cuci', 'color' => '#a7a1d9','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 14, 'store_id' => 1, 'name' => 'Roti Kering & Basah', 'color' => '#b5dd22','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
+            array('id' => 15, 'store_id' => 1, 'name' => 'Snack', 'color' => '#7a281c','created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03')
         );
         
         DB::table('categories')->insert($categories);
--- database/seeds/A004_ProductsSeeder.php
+++ database/seeds/A004_ProductsSeeder.php
@@ -14,7 +14,21 @@ class A004_ProductsSeeder extends Seeder
     {
         $products = array(
             array('id' => 1, 'category_id' => 1, 'name' => 'Es Teh', 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
-            array('id' => 2, 'category_id' => 2, 'name' => 'Lemper', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04')
+            array('id' => 2, 'category_id' => 14, 'name' => 'Lemper', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 3, 'category_id' => 2, 'name' => 'Beras', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 4, 'category_id' => 3, 'name' => 'Wajan', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 5, 'category_id' => 4, 'name' => 'Jam Dinding', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 6, 'category_id' => 5, 'name' => 'Sapu Lidi', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 7, 'category_id' => 6, 'name' => 'Minyak Telon', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 8, 'category_id' => 7, 'name' => 'Sikat WC', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 9, 'category_id' => 8, 'name' => 'Minyak Rambut', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 10, 'category_id' => 9, 'name' => 'Obat Flu', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 11, 'category_id' => 10, 'name' => 'Minyak Goreng', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 12, 'category_id' => 11, 'name' => 'Daging Ayam', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 13, 'category_id' => 12, 'name' => 'Penggaris', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 14, 'category_id' => 13, 'name' => 'Detergen', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 15, 'category_id' => 15, 'name' => 'Snack A', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 16, 'category_id' => 15, 'name' => 'Snack B', 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04')
         );
 
         foreach($products as $product)
