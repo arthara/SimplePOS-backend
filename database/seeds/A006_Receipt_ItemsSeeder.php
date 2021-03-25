44e7a37ccc33ba4d57d8fd36983f3df6b276ec73
ARTHARA <the.first678@gmail.com>
2021-03-25 15:29:15 +0700

    Update Seeder (#38)

--- database/seeds/A006_Receipt_ItemsSeeder.php
+++ database/seeds/A006_Receipt_ItemsSeeder.php
@@ -15,7 +15,9 @@ class A006_Receipt_ItemsSeeder extends Seeder
             array('id' => 1, 'receipt_id' => 1, 'product_history_id' => 1, 'unit_total' => 1, 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
             array('id' => 2, 'receipt_id' => 1, 'product_history_id' => 2, 'unit_total' => 2, 'created_at' => '2021-02-05 15:09:02','updated_at' => '2021-02-05 15:09:03'),
             array('id' => 3, 'receipt_id' => 2, 'product_history_id' => 1, 'unit_total' => 2, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
-            array('id' => 4, 'receipt_id' => 2, 'product_history_id' => 2, 'unit_total' => 1, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04')
+            array('id' => 4, 'receipt_id' => 2, 'product_history_id' => 2, 'unit_total' => 1, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 5, 'receipt_id' => 3, 'product_history_id' => 3, 'unit_total' => 5, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04'),
+            array('id' => 6, 'receipt_id' => 3, 'product_history_id' => 1, 'unit_total' => 2, 'created_at' => '2021-02-05 15:09:03','updated_at' => '2021-02-05 15:09:04')
         );
 
         DB::table('receipt_items')->insert($receipt_items);
